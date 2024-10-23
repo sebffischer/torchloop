ba0babacd5641eeb792cf30b151d7c621bebf819
Sebastian Fischer <sebf.fischer@gmail.com>
2024-10-23 11:41:37 +0200

    ...

--- build/CMakeFiles/lltmExport.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lltmExport"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lltmExport.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/lltmExport.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for lltmExport.
-# This may be replaced when dependencies are built.
